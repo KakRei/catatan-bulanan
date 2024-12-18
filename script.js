d784a5f55944c3a70ba38a7104f4c9b6c4e80406
Raihan Aditya <ra.raihanaditya@gmail.com>
2024-12-19 02:53:44 +0700

    can add more than 1 row data

--- script.js
+++ script.js
@@ -3,18 +3,23 @@ function addData() {
     const dateInput = document.getElementById('input-date');
     const incomeInput  = document.getElementById('input-income');
     const expendInput = document.getElementById('input-expend');
-
+    
     const date = dateInput.value;
     const income = incomeInput.value;
     const expend = expendInput.value;
     
-    const tDate = document.getElementById('table-date');
-    const tIncome = document.getElementById('table-income');
-    const tExpend = document.getElementById('table-expend');
-
-    tDate.textContent = date
-    tIncome.textContent = income
-    tExpend.textContent = expend
+    const table = document.getElementById('table');
+    
+    const row = table.insertRow(-1),
+    dateCell = row.insertCell(0),
+    incomeCell = row.insertCell(1),
+    expendCell = row.insertCell(2);
+    
+    const formatter = new Intl.NumberFormat('Id-id');
+    
+    dateCell.textContent = date;
+    incomeCell.textContent = formatter.format(income);
+    expendCell.textContent = formatter.format(expend);
 
     dateInput.value = '';
     incomeInput.value = '';
