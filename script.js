45caee1927d5eb666dea00dad8eaf9510d3d1d0a
Raihan Aditya <ra.raihanaditya@gmail.com>
2024-12-22 14:25:44 +0700

    the items is stored in local storage

--- script.js
+++ script.js
@@ -1,51 +1,11 @@
-// const table = document.getElementById('table');
 
-// function addData() {
-//     const dateInput = document.getElementById('input-date');
-//     const incomeInput  = document.getElementById('input-income');
-//     const expendInput = document.getElementById('input-expend');
-    
-//     const date = dateInput.value;
-//     const income = incomeInput.value;
-//     const expend = expendInput.value;
-
-    
-//     localStorage.setItem("date", date);
-//     localStorage.setItem("income", income);
-//     localStorage.setItem("expend", expend);
-    
-    
-//     dateInput.value = '';
-//     incomeInput.value = '';
-//     expendInput.value = '';
-
-//     getData();
-//     return
-// }
-
-// function getData() {
-//     const date = localStorage.getItem("date");
-//     const income = localStorage.getItem("income");
-//     const expend = localStorage.getItem("expend");
-
-//     const row = table.insertRow(-1),
-//     dateCell = row.insertCell(0),
-//     incomeCell = row.insertCell(1),
-//     expendCell = row.insertCell(2);
-    
-//     const formatter = new Intl.NumberFormat('Id-id');
-    
-//     dateCell.textContent = date;
-//     incomeCell.textContent = formatter.format(income);
-//     expendCell.textContent = formatter.format(expend);
-// }
-
-// getData();
-
+getData();
 function addData() {
     const dateInput = document.getElementById('input-date');
     const incomeInput  = document.getElementById('input-income');
     const expendInput = document.getElementById('input-expend');
+
+    const tableBody = document.getElementById('table-body');
     
     const date = dateInput.value;
     const income = incomeInput.value;
@@ -58,21 +18,19 @@ function addData() {
         expend: expend
     };
 
-    // Get existing data from local storage
-    let data = JSON.parse(localStorage.getItem("data")) || [];
+    let data = JSON.parse(localStorage.getItem('data')) || [];
 
-    // Add new data entry to the array
     data.push(dataEntry);
 
-    // Store the updated data array in local storage
-    localStorage.setItem("data", JSON.stringify(data));
-    
+    localStorage.setItem('data', JSON.stringify(data));
+
     dateInput.value = '';
     incomeInput.value = '';
     expendInput.value = '';
 
+    tableBody.innerHTML = '';
+
     getData();
-    return
 }
 
 function getData() {
@@ -95,5 +53,3 @@ function getData() {
         expendCell.textContent = formatter.format(entry.expend);
     }
 }
-
-getData();
